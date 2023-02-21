15915ba0575e41b20efb68cec5636885ce1d4f88
Alejandra Guzman <77361246+guzmanalejandra@users.noreply.github.com>
2023-02-21 11:09:51 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='bayesiannnetwork',
+ version='0.2',
+ author="Alejandra Guzmán",
+ author_email="guzmandominguez.ale@gmail.com",
+ description="Paquete para crear redes bayesianas simples",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
